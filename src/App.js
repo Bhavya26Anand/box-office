4ef95120b200bf2c05cdd6d48d1e5a02c7e5f3ae
Bhavya Anand <bhavya26anand@gmail.com>
2022-07-10 14:53:26 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Starred from './pages/Starred';
 import Home from './pages/Home';
+import Starred from './pages/Starred';
+import Show from './pages/Show';
 
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
 
-        <Route>
-          <div>404 error</div>
-        </Route>
-      </Switch>
-    </div>
+      <Route exact path="/show/:id">
+        <Show />
+      </Route>
+
+      <Route>
+        <div>Not found</div>
+      </Route>
+    </Switch>
   );
 }
 
